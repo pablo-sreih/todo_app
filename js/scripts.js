03d59a1c2dcafca63006cd51c29e4f392ad66e50
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-05-30 09:21:57 +0300

    Fixed Description Field Not Appending

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $("#add").click(function () {
     let title = $("#title").val();
-    let desc = $("#description");
+    let desc = $("#description").val();
+
     var todo = `<div id="list-container">
     <form id="form-container">
         <input maxlength="30" id="title" type="text" value="${title}" readonly/>
@@ -8,18 +9,19 @@ $("#add").click(function () {
           id="description"
           cols="30"
           rows="4"
-          value = "${desc}"
-        ></textarea>
+          readonly
+        >${desc}</textarea>
         <div id="footer-container">
             <label id="date">28 05 2022</label>
             <div>
-                <button id="edit">Edit</button>
-                <button id="delete">Delete</button>
+                <button type="button" id="edit">Edit</button>
+                <button type="button" id="delete">Delete</button>
             </div>
             </div>
         </div>
       </form>
 </div>`;
+
     if (title == "" || desc == ""){
         alert("Empty");
     } else {
