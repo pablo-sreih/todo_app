2e83b25a0e68ebd7726f611b1294a8035cecdc87
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-05-30 08:44:55 +0300

    Added Todo Append Function

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$("#add").click(function () {
+    let title = $("#title").val();
+    let desc = $("#description");
+    var todo = `<div id="list-container">
+    <form id="form-container">
+        <input maxlength="30" id="title" type="text" value="${title}" readonly/>
+        <textarea
+          id="description"
+          cols="30"
+          rows="4"
+          value = "${desc}"
+        ></textarea>
+        <div id="footer-container">
+            <label id="date">28 05 2022</label>
+            <div>
+                <button id="edit">Edit</button>
+                <button id="delete">Delete</button>
+            </div>
+            </div>
+        </div>
+      </form>
+</div>`
+    $("body").append(todo)
+    console.log("success")
+})
