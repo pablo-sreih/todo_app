c8e18a7f4941ddc3bf08cc7699c499eb258b0d1a
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-06-02 23:51:45 +0300

    Added Edit Button Functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,55 @@
-$("#add").click(function () {
+$(document).ready(() => {
+
+var id = 0;
+
+
+$("#add").click(() => {
     let title = $("#title").val();
-    let desc = $("#description").val();
+    let description = $("#description").val();
+    var todo = new List(title, description)
+    todo.add_todo();
+    console.log(todo)
+})
 
-    var todo = `<div id="list-container">
-    <form id="form-container">
-        <input maxlength="30" id="title" type="text" value="${title}" readonly/>
-        <textarea
-          id="description"
-          cols="30"
-          rows="4"
-          readonly
-        >${desc}</textarea>
-        <div id="footer-container">
-            <label id="date">28 05 2022</label>
-            <div>
-                <button type="button" id="edit">Edit</button>
-                <button type="button" id="delete">Delete</button>
-            </div>
-            </div>
-        </div>
-      </form>
-</div>`;
 
-    if (title == "" || desc == ""){
-        alert("Empty");
-    } else {
-    $("body").append(todo);
-    console.log("success");
+class List {
+
+    constructor(title, description){
+        this.id = id++;
+        this.title = title;
+        this.description = description;
+        this.div = $(`<div id="${this.id}"></div>`);
+        var today = new Date();
+        this.time = `${today.getDay()}/${today.getMonth()}/${today.getFullYear()}  ${today.getHours()}:${today.getMinutes()}`;
     }
+
+
+    add_todo() {
+        let todo_div = `<div id="list-container">
+        <form id="form-container">
+            <input maxlength="30" class="title" id="title${this.id}" type="text" placeholder="Title" value = "${this.title}"readonly/>
+            <textarea maxlength="150" name="description" id="description" cols="30" rows="4" placeholder="Description">${this.description}</textarea>
+            <div id="footer-container">
+                <label id="date">${this.time}</label>
+                <div>
+                    <button type="button" class="edit" id="edit${this.id}">Edit</button>
+                    <button type="button" class="delete" id="delete${this.id}">Delete</button>
+                </div>
+                </div>
+            </div>
+          </form>`;
+
+
+        $("body").append(this.div.append(todo_div));
+
+
+        $(".edit").click(() => {
+            console.log("clicked")
+            document.getElementById(`title${this.id}`).removeAttribute("readonly");
+
+        })
+
+    }
+    }
+
 })
